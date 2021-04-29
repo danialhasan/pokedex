1d7f866861fefed47d2749dbac786ddf5eafa2b6
Danial Hasan <danialhasan14@gmail.com>
2021-04-29 00:33:34 -0400

    last push was dev build; this is production build.

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,6 @@
 <script>
 import PokedexParent from "./components/PokedexParent";
 
-/**
- * TODO
- * [] Fetch data from API to populate top level components' database
- * [] Loop through data. For each data object, produce one pokedex instance.
- *    Pass in relevant data through props.
- */
 export default {
   name: "App",
   components: {
